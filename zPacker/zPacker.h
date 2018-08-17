4b7e47b25bde5092a397f8099da16384d3439a7f
wwt3100 <309246295@qq.com>
2018-08-17 17:25:59 +0800

    Initial commit

---
+++ zPacker/zPacker.h
@@ -0,0 +1,32 @@
+
+// zPacker.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CzPackerApp: 
+// 有关此类的实现，请参阅 zPacker.cpp
+//
+
+class CzPackerApp : public CWinApp
+{
+public:
+	CzPackerApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CzPackerApp theApp;
